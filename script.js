9502f24a991465ae14341323affb1064643f2f26
Frownigami1 <sam.o.l.williams94@gmail.com>
2018-04-17 11:48:48 +0100

    Level 1 tasks commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/*function changeVis() {
+	document.getElementById("dropmenu").style.visibility = "hidden";
+}*/
+
+function myFunction() {
+	document.getElementById("demo").innerHTML = "changed";
+}
+
+function dropMenu() {
+	
+}
